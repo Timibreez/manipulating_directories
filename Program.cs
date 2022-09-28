ce2e779ee85d24ecb61754bf61642ec551943afd
Timibreez <ffftimi@gmail.com>
2022-09-28 10:38:25 +0100

    added Comments to ensure easier code readability

--- Program.cs
+++ Program.cs
@@ -21,12 +21,18 @@ IEnumerable<string> FindFiles(string folderName){
 
     return salesFiles;
 }
-
+// Get current working directory
 Console.WriteLine(Directory.GetCurrentDirectory());
 
+// Find path of specific direcotry
 string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 Console.WriteLine(docPath);
 
+// Get corresponding path to file, directory irrespective of operating system convention
 Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
 
+// Find paths using conventional joining 
 Console.WriteLine(Path.Combine("stores", "203"));
+
+// Determine file extension
+Console.WriteLine(Path.GetExtension("sales.json"));
