31eb8142504266829aaf4321c7d7a0b8ddab2220
Timibreez <ffftimi@gmail.com>
2022-09-28 10:02:39 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var requiredFiles = FindFiles("stores");
+
+foreach(var file in requiredFiles){
+    Console.WriteLine(file);
+}
+
+Console.WriteLine("Hello, World!");
+
+IEnumerable<string> FindFiles(string folderName){
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach(var file in foundFiles){
+        if (file.EndsWith("sales.json")){
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
